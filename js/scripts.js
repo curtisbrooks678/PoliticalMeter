16cf664c5978e25deef247db92660df286867906
Liam Eller <liamthelastson@gmail.com>
2021-10-21 11:45:48 -0700

    Add a required funtion to the form input
    Co-Authored-by: Skylar Brockbank <skylar.brockbank@gmail.com>
    Co-authored-by: Curtis Brooks <curtisbrooks678@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// $(document).ready(function() {
+//   $("#form1").submit(function(event) {
+//     if (sessionStorage.checkValidityState()===false) {
+//       event.preventDefault();
+//       event.stopPropogation();
+//     }
+//   });
+// });
